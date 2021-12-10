50fbb14aefe1b3651f68527b5686de058be19bd1
“Pedro <padronpedro@gmail.com>
2021-12-10 15:00:41 -0500

    git update version

---
+++ src/components/counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="counter">
+    <button @click="counter++">Click me</button>
+    {{ counter }}
+  </div>
+</template>
+
+<script>
+export default {
+  data: () =>({
+    counter: 0
+  })
+}
+</script>
